e0f3a2287f4becc3724c5b7247e6702fc9b5c999
Vicky <o30329o@gmail.com>
2024-06-04 11:15:46 +0800

    New HW file

---
+++ web113a/project/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("navbar-toggle").addEventListener("click", function () {
+  const menu = document.querySelector(".navbar-menu");
+  menu.classList.toggle("active");
+});
